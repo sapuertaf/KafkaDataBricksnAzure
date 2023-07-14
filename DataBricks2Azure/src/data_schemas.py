2a82502b9184f8a0d626166140ca1de851887f87
spuerta10 <puertasantiago2002@gmail.com>
2023-07-13 21:17:41 -0500

    feat(data schemas): added data schemas for customer, date, product, reseller & order.

---
+++ DataBricks2Azure/src/data_schemas.py
@@ -0,0 +1,49 @@
+from pyspark.sql.types import StructType, IntegerType,StringType,FloatType
+
+schema_customer = StructType()\
+                  .add("CustomerKey",IntegerType())\
+                  .add("CustomerID",StringType())\
+                  .add("Customer",StringType())\
+                  .add("City",StringType())\
+                  .add("StateProvince",StringType())\
+                  .add("CountryRegion",StringType())\
+                  .add("PostalCode",StringType())
+
+
+schema_date = StructType()\
+              .add("DateKey",IntegerType())\
+              .add("Date",StringType())\
+              .add("FiscalYear",StringType())\
+              .add("FiscalQuarter",StringType())\
+              .add("Month",StringType())\
+              .add("FullDate",StringType())\
+              .add("MonthKey",IntegerType())
+
+schema_product = StructType()\
+                 .add("ProductKey",IntegerType())\
+                 .add("SKU",StringType())\
+                 .add("Product",StringType())\
+                 .add("StandardCost",FloatType())\
+                 .add("Color",StringType())\
+                 .add("ListPrice",FloatType())\
+                 .add("Model",StringType())\
+                 .add("Subcategory",StringType())\
+                 .add("Category",StringType())
+
+schema_reseller = StructType()\
+                 .add("ResellerKey",IntegerType())\
+                 .add("ResellerID",StringType())\
+                 .add("BusinessType",StringType())\
+                 .add("Reseller",StringType())\
+                 .add("City",StringType())\
+                 .add("StateProvince",StringType())\
+                 .add("CountryRegion",StringType())\
+                 .add("PostalCode",StringType())
+
+schema_sales_order = StructType()\
+                 .add("Channel",StringType())\
+                 .add("SalesOrderLineKey",IntegerType())\
+                 .add("SalesOrder",StringType())\
+                 .add("SalesOrderLine",IntegerType())
+
+schema_sales_territory = ""
