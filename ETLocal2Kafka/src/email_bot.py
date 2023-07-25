72fbf20d2f7164cbc9c77f256fede538e977821d
Santiago Puerta <puertasantiago2002@gmail.com>
2023-07-25 17:14:39 -0500

    feat(email bot docs): Added PyDoc to email bot methods

--- ETLocal2Kafka/src/email_bot.py
+++ ETLocal2Kafka/src/email_bot.py
@@ -27,14 +27,25 @@ class EmailSenderBot:
 
 
     @typechecked
-    def set_mail_receptor(self, email_receptor:str):
+    def set_mail_receptor(self, email_receptor:str) -> None:
+        """Establecer al bot la direccion de correo donde sera enviado el mail. 
+
+        Args:
+            email_receptor (str): Correo destino donde sera enviado el correo.
+        """
         assert validate_email(email_receptor, verify=True), "La direccion de correo para el receptor dada es invalida"
         self.receptor = email_receptor
 
 
 
     @typechecked
-    def struct_message(self, email_subject:str, body:str=""):
+    def struct_message(self, email_subject:str, body:str="") -> None:
+        """Estructurar el correo a ser enviado.
+
+        Args:
+            email_subject (str): Asunto del correo.
+            body (str, optional): Cuerpo del correo. Defaults to "".
+        """
         self.email = MIMEMultipart() #se deben de crear tantos contextos de email como emails se deseen enviar
         self.email["To"] = self.receptor
         self.email["From"] = self.sender
@@ -43,14 +54,25 @@ class EmailSenderBot:
 
 
 
-    def send(self):
-        #uso de SSM para tener una conexion segura al enviar el correo
+    def send(self) -> int:
+        """Enviar correo a la direccion destino 
+
+        Returns:
+            int: Resultado de envio del correo. 
+                 0 en caso de exito. 1 en caso de error. 
+        """
+        #uso de SSL para tener una conexion segura al enviar el correo
         context_ = ssl.create_default_context()
         with smtplib.SMTP_SSL(MAIL_SERVER, PORT, context=context_) as smtp:
-            if isinstance(self.sender, tuple):
-                smtp.login(*self.sender, self.sender_password) #logearse desempaquetando la tupla
-            smtp.login(self.sender, self.sender_password) #logearse
-            smtp.sendmail(self.sender, self.receptor, self.email.as_string())
+            try:
+                if isinstance(self.sender, tuple):
+                    smtp.login(*self.sender, self.sender_password) #logearse desempaquetando la tupla
+                smtp.login(self.sender, self.sender_password) #logearse
+                smtp.sendmail(self.sender, self.receptor, self.email.as_string())
+                return 0
+            except Exception as error:
+                print(error)
+                return 1
 
 
 
