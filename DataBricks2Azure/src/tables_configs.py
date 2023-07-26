c4f45e44f063b5cce73463ecc1edae664dab86e6
spuerta10 <puertasantiago2002@gmail.com>
2023-07-26 02:08:44 -0500

    feat(config dicr transform columns): Added dictionary relating table name and columns in need of transformation and wich type of transformation

---
+++ DataBricks2Azure/src/tables_configs.py
@@ -0,0 +1,12 @@
+columns_types_2_transform:list = ["price", "percentage"]
+
+global_confs = {
+    "sales" : {
+      "ExtendedAmount":columns_types_2_transform[0],
+      "UnitPrice":columns_types_2_transform[0],
+      "ProductStandardCost":columns_types_2_transform[0],
+      "TotalProductCost":columns_types_2_transform[0],
+      "SalesAmount":columns_types_2_transform[0],
+      "UnitPriceDiscountPct" :columns_types_2_transform[1],  
+    },
+}
